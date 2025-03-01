3d94a82483ed9df5f3246ac05b19483c8f46d234
edwardsdean <edwardsdean@hotmail.com>
2025-03-01 23:04:16 +1100

    Update and rename winget to winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,22 @@
+winget install -e --id ffmpeg
+winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
+winget install -e --id Discord.Discord
+winget install -e --id Valve.Steam
+winget install -e --id Notepad++.Notepad++
+winget install -e --id PuTTY.PuTTY
+winget install -e --id Python.Python.3.12
+winget install -e --id JetBrains.PyCharm.Community
+winget install -e --id SublimeHQ.SublimeText.4
+winget install -e --id VideoLAN.VLC
+winget install -e --id WinSCP.WinSCP
+winget install -e --id AntibodySoftware.WizTree
+winget install -e --id Adobe.Acrobat.Reader.64-bit
+
+iwr -useb https://christitus.com/win | iex
+
+
+
+
+
+
+https://github.com/yt-dlp/yt-dlp
