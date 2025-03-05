b62b73f0f131aa4aab505b9049f7baaf8afa3d5d
edwardsdean <edwardsdean@hotmail.com>
2025-03-05 11:05:24 +1100

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,16 +1,34 @@
-winget install -e --id ffmpeg
+winget install -e --id M2Team.NanaZip
 winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
-winget install -e --id Discord.Discord
-winget install -e --id Valve.Steam
+winget install -e --id Google.Chrome
+winget install -e --id Mozilla.Firefox
+
+
 winget install -e --id Notepad++.Notepad++
 winget install -e --id PuTTY.PuTTY
-winget install -e --id Python.Python.3.12
-winget install -e --id JetBrains.PyCharm.Community
+
+
+
 winget install -e --id SublimeHQ.SublimeText.4
 winget install -e --id VideoLAN.VLC
 winget install -e --id WinSCP.WinSCP
 winget install -e --id AntibodySoftware.WizTree
 winget install -e --id Adobe.Acrobat.Reader.64-bit
+
+
+## Dean PC only
+
+winget install -e --id Discord.Discord
+winget install -e --id Valve.Steam
+winget install -e --id EpicGames.EpicGamesLauncher
+
+winget install -e --id ffmpeg
+winget install -e --id Python.Python.3.12
+winget install -e --id JetBrains.PyCharm.Community
+
+
+## Dean Desktop Only
+winget install -e --id Guru3D.Afterburner
 
 iwr -useb https://christitus.com/win | iex
 
